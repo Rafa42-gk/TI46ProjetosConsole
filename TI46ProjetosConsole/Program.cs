4e26019d676d2fcae77785ae2933f549b3e8effa
RAFAEL LUIS MARQUEZIN <rafael.lmarquezin@senacsp.edu.br>
2024-09-30 22:16:40 -0300

    Aplicativo 1, notas e nome

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -10,8 +10,33 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
-            Console.WriteLine("teste , Linha 2");
+            string nome;
+            int nota1, nota2, nota3, nota4, Resultado;
+            Console.WriteLine("Digite seu nome: ");
+            nome = (Console.ReadLine());
+            Console.WriteLine("Seja Bem-vindo, ", nome);
+
+            //Entre com as Notas
+            Console.WriteLine("Digite a 1° nota do aluno: ", nome);
+            nota1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a 2° nota do aluno: ", nome);
+            nota2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a 3° nota do aluno: ", nome);
+            nota3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a 4° nota do aluno: ", nome);
+            nota4 = int.Parse(Console.ReadLine());
+
+            //processamento
+            Resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            //saída
+            Console.WriteLine("O aluno " + nome + " teve média final: ", + Resultado);
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }
